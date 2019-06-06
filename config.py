151dba42141afd345c56e5bce71b83a9055e97d1
DeeKay912 <dominikklein@outlook.de>
2019-06-06 16:54:27 +0200

    update main, conf and reader

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 # You CAN add new config parameters
 # Create a new file calling config_dev.py and copy all config names to it. You can edit then config_dev.py.
 # Do NOT add config_dev.py to git
-DATAFILE = ''
+LOADPATH = ''
+SAVEPATH = ''
--- main.py
+++ main.py
@@ -1,44 +1,62 @@
-import Reader as reader
-import numpy as np
+import os
+
+import math
 import matplotlib.pyplot as plt
 import numpy as np
-import config_dev as cfg
-import math
 import peakutils
 from scipy import signal
 
-DATAFILE = '2019-05-2711.00.19.csv'
+import Reader as reader
+import config_dev as cfg
 
+DATAFILE = 'record1.csv'
+LOWPASSFILTER = True
+filename = os.path.splitext(os.path.basename(DATAFILE))[0]
 
 # ----- Load the data to a numpy array and save the array
-data = reader.readCSVData(DATAFILE)
-np.save(cfg.SAVEPATH + 'dataLoaded.npy', data)
-data = np.load(cfg.SAVEPATH + 'dataLoaded.npy')
+filepath = cfg.SAVEPATH + filename + '_dataLoaded.npy'
+if not os.path.exists(filepath):
+    if not os.path.exists(cfg.SAVEPATH):
+        os.makedirs(cfg.SAVEPATH, exist_ok=True)
+    print('numpy array does not exist yet')
+    data = reader.readCSVData(DATAFILE)
+    np.save(filepath, data)
+
+# Load data from array
+data = np.load(filepath)
 
 # ----- Visualize the data
 # time,gFx.gFy,gFz.wx,wy.wz,
 fig, axs = plt.subplots(3, 2)
+# G-force data
+# Plot data from first and second column
 axs[0, 0].plot(data[:, 0], data[:, 1])
 axs[0, 0].set_xlabel('time')
 axs[0, 0].set_ylabel('gFx')
 axs[0, 0].grid(True)
+# Plot data from first and third column
 axs[1, 0].plot(data[:, 0], data[:, 2])
 axs[1, 0].set_xlabel('time')
 axs[1, 0].set_ylabel('gFy')
 axs[1, 0].grid(True)
+# Plot data from first and fours column
 axs[2, 0].plot(data[:, 0], data[:, 3])
 axs[2, 0].set_xlabel('time')
 axs[2, 0].set_ylabel('gFz')
 axs[2, 0].grid(True)
 
+# Gyroscope data
+# Plot data from first and fifth column
 axs[0, 1].plot(data[:, 0], data[:, 4])
 axs[0, 1].set_xlabel('time')
 axs[0, 1].set_ylabel('wx')
 axs[0, 1].grid(True)
+# Plot data from first and sixth column
 axs[1, 1].plot(data[:, 0], data[:, 5])
 axs[1, 1].set_xlabel('time')
 axs[1, 1].set_ylabel('wy')
 axs[1, 1].grid(True)
+# Plot data from first and seventh column
 axs[2, 1].plot(data[:, 0], data[:, 6])
 axs[2, 1].set_xlabel('time')
 axs[2, 1].set_ylabel('wz')
@@ -56,8 +74,8 @@ for x in range(0, dataLength[0]):
 
 data = np.append(data, dataAppend, axis = 1)
 # Save new Data
-np.save(cfg.SAVEPATH + 'dataWithAccNorm.npy', data)
-data = np.load(cfg.SAVEPATH + 'dataWithAccNorm.npy')
+np.save(cfg.SAVEPATH + filename + '_dataWithAccNorm.npy', data)
+data = np.load(cfg.SAVEPATH + filename + '_dataWithAccNorm.npy')
 
 # Plot new Data
 plt.figure()
@@ -76,7 +94,7 @@ plt.plot(data[indexes,0], data[indexes, 7], 'ro')
 plt.show()
 
 
-if False:
+if LOWPASSFILTER:
     # ---- First try of a low pass filter
     fc = 3  # Cut-off frequency of the filter
     fs = 400  # Sampling frequency
@@ -92,6 +110,17 @@ if False:
     indexes2 = peakutils.indexes(output, thres=0.4 * max(output), min_dist=50)
     print('Anzahl peaks (nach Filter):', indexes2.shape[0])
     plt.plot(data[indexes2,0], output[indexes2], 'ro')
-    plt.show()
+    #plt.show()
 
-import AngularIntegration
+    # ---- extract peaks by using scipy.signal
+    avg = np.mean(output)
+    print('Average height: ', avg)
+    print('Min height of peak: ', 1.1 * avg)
+    [idx, prop] = signal.find_peaks(output, height=1.1*avg)
+    print('Anzahl Peaks mit scipy + Filter: ', len(idx))
+    plt.figure()
+    plt.xlabel('time')
+    plt.ylabel('Acc.')
+    plt.plot(data[:, 0], output, label='filtered')
+    plt.plot(data[idx, 0], output[idx], 'ro')
+    plt.show()
